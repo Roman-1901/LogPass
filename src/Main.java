e6eda50071e5170dc35ebf7d9c6bc681308fced8
roman <roman-1901@mail.ru>
2023-01-04 20:24:34 +0300

    q

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner sc1 = new Scanner(System.in);
-//        System.out.println("Введите логин");
-//        String login =  sc1.nextLine();
-//
-//        Scanner sc2= new Scanner(System.in);
-//        System.out.println("Введите пароль");
-//        String password =  sc2.nextLine();
-//
-//        Scanner sc3= new Scanner(System.in);
-//        System.out.println("Подтвердите пароль");
-//        String confirmPassword =  sc3.nextLine();
-//
-//        LoginPassword user = new LoginPassword(login, password, confirmPassword);
-//        System.out.println(user);
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите логин");
+        String login =  sc1.nextLine();
+
+        Scanner sc2= new Scanner(System.in);
+        System.out.println("Введите пароль");
+        String password =  sc2.nextLine();
+
+        Scanner sc3= new Scanner(System.in);
+        System.out.println("Подтвердите пароль");
+        String confirmPassword =  sc3.nextLine();
 
 
-
-        LogPas user = new LogPas("Dimon123", "Password123", "Password123");
+        LogPas user = new LogPas(login, password, confirmPassword);
     }
 }
